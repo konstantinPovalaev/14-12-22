5f10d8e8f61973f56113b580134b78857627f76d
Konstantin <kpovalaev7@gmail.com>
2022-12-21 19:31:25 +0300

    egw

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" :name="name" :phone="phone" :photo="photo"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'App'
   components: {
     HelloWorld
+  }
+  one(){
+    name : "",
+    phone: "",
+    photo: ""
+  }
+  two(){
+    this.axios.get('https://randomuser.me/api/')         
+    .then((response) => {
+      console.log(response.data),
+      this.name = "kosty" 
+      this.phone = "89295856468" 
+      this.photo ="aa"    
+    }) 
   }
 }
 </script>
